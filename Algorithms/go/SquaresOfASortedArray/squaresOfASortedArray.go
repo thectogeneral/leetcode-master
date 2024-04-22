a5017115a42cb5b5c6f8dd31107bf7c7a536821a
Salem Olorundare <salemolorundare@gmail.com>
2024-04-22 17:35:17 +0100

    implemented solution to square of sorted array in golang

---
+++ Algorithms/go/SquaresOfASortedArray/squaresOfASortedArray.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func sortedSquares(nums []int) []int {
+	// Map each element 'a' in slice 'nums' to its square and store the result in a new slice
+	// Sort the new slice in non-decreasing order
+	// Return the sorted slice
+	squared := make([]int, len(nums))
+	for i, num := range nums {
+		squared[i] = num * num
+	}
+	sort.Ints(squared)
+	return squared
+}
+
+func main() {
+	nums := []int{-4, -1, 0, 3, 10}
+	sortedSquaresArray := sortedSquares(nums)
+	fmt.Println(sortedSquaresArray) // Output: [0 1 9 16 100]
+}
